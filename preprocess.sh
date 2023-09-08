366e4dffc75f446cbf80f0ec495dd280124ebf1c
Junyong Shin <jun021222@naver.com>
2023-09-08 03:29:34 -0400

    Update bashscripts

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,15 @@
+data_path=/workspace/Deep3dPortrait/input/doo
+
 conda activate face_alignment
 
-python scripts/preprocess.py
+python scripts/landmark.py $data_path # generate facial landmarks
+
+# generate face parsing
+cd third_party/face-parsing.PyTorch
+python test.py $data_path
+cd -
+
+# convert face parsing labels
+python scripts/convert_mask.py $data_path/mask
 
 conda deactivate
